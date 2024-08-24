417a06f6fbb4f25a480e5ccd9007594cd31de6b8
mustafaberataru <mustafa.ceren.18@hotmail.com>
2024-08-24 20:21:51 +0300

    typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>WhatsApp Instant Message</title>
         {/* Favicon and Icons */}
